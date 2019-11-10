2617fcca6a9ff00808265f7c0aac959bf05bbb5b
ryo <ba068082@gmail.com>
2019-11-11 06:55:08 +0900

    [from now] 2019/11/11 06:55:08
    diff --git a/functions/package.json b/functions/package.json
    index ef03483..0df107f 100644
    --- a/functions/package.json
    +++ b/functions/package.json
    @@ -3,7 +3,7 @@
       "scripts": {
         "lint": "tslint --project tsconfig.json",
         "build": "tsc",
    -    "serve": "npm run build && firebase serve --only functions",
    +    "serve": "npm run build && firebase serve --only functions,firestore",
         "shell": "npm run build && firebase functions:shell",
         "start": "npm run shell",
         "deploy": "firebase deploy --only functions",
    @@ -15,6 +15,8 @@
       },
       "main": "lib/index.js",
       "dependencies": {
    +    "date-utils": "^1.2.21",
    +    "dayjs": "^1.8.17",
         "firebase-admin": "^8.0.0",
         "firebase-functions": "^3.1.0"
       },
    diff --git a/functions/src/index.ts b/functions/src/index.ts
    index 39c3304..46b27ae 100644
    --- a/functions/src/index.ts
    +++ b/functions/src/index.ts
    @@ -1,5 +1,41 @@
     import * as functions from 'firebase-functions';
    +import * as admin from 'firebase-admin';
    +import * as dayjs from "dayjs";
    +
    +admin.initializeApp();
    +
    +const db = admin.firestore()
    
     export const helloWorld = functions.https.onRequest((request, response) => {
       response.send("Hello from Firebase!");
     });
    +
    +export const dash1 = functions.https.onRequest(async (request, response) => {
    +  const insertTime = Date.now();
    +  await db.collection("dash1").add({
    +    createdAt: insertTime
    +  });
    +
    +  // 2秒待機
    +  await new Promise((resolve) => setTimeout(() => resolve(), 3000));
    +
    +  // その後のリクエストを取得
    +  const resentDocs = await db.collection("dash1").orderBy("createdAt", "desc").limit(3).get();
    +
    +  // もし後にリクエストがあったら特に動作なし
    +  const followingRequest = resentDocs.docs.filter((doc) => {
    +    return dayjs(doc.data().createdAt).isAfter(dayjs(insertTime))
    +  });
    +
    +  if(followingRequest.length > 0) {
    +    response.send("特に動作なし");
    +    return;
    +  }
    +
    +  // 前のリクエストから動作を検証する
    +  const previousRequests = resentDocs.docs.filter((doc) => dayjs(doc.data().createdAt).isBefore(dayjs(insertTime).subtract(3, "second")));
    +
    +  // リクエストがなかったら
    +  response.send(previousRequests.length.toString());
    +});
    +
    diff --git a/functions/yarn.lock b/functions/yarn.lock
    index 8f15c35..5830a7c 100644
    --- a/functions/yarn.lock
    +++ b/functions/yarn.lock
    @@ -1248,6 +1248,16 @@ date-and-time@^0.10.0:
       resolved "https://registry.yarnpkg.com/date-and-time/-/date-and-time-0.10.0.tgz#53825b774167b55fbdf0bbd0f17f19357df7bc70"
       integrity sha512-IbIzxtvK80JZOVsWF6+NOjunTaoFVYxkAQoyzmflJyuRCJAJebehy48mPiCAedcGp4P7/UO3QYRWa0fe6INftg==
    
    +date-utils@^1.2.21:
    +  version "1.2.21"
    +  resolved "https://registry.yarnpkg.com/date-utils/-/date-utils-1.2.21.tgz#61fb16cdc1274b3c9acaaffe9fc69df8720a2b64"
    +  integrity sha1-YfsWzcEnSzyayq/+n8ad+HIKK2Q=
    +
    +dayjs@^1.8.17:
    +  version "1.8.17"
    +  resolved "https://registry.yarnpkg.com/dayjs/-/dayjs-1.8.17.tgz#53ec413f2a7b02afbea1846d61bb260fa8567cea"
    +  integrity sha512-47VY/htqYqr9GHd7HW/h56PpQzRBSJcxIQFwqL3P20bMF/3az5c3PWdVY3LmPXFl6cQCYHL7c79b9ov+2bOBbw==
    +
     debug@2.6.9, debug@^2.2.0, debug@^2.3.3:
       version "2.6.9"
       resolved "https://registry.yarnpkg.com/debug/-/debug-2.6.9.tgz#5d128515df134ff327e90a4c93f4e077a536341f"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,41 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as dayjs from "dayjs";
+
+admin.initializeApp();
+
+const db = admin.firestore()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+export const dash1 = functions.https.onRequest(async (request, response) => {
+  const insertTime = Date.now();
+  await db.collection("dash1").add({
+    createdAt: insertTime
+  });
+
+  // 2秒待機
+  await new Promise((resolve) => setTimeout(() => resolve(), 3000));
+
+  // その後のリクエストを取得
+  const resentDocs = await db.collection("dash1").orderBy("createdAt", "desc").limit(3).get();
+
+  // もし後にリクエストがあったら特に動作なし
+  const followingRequest = resentDocs.docs.filter((doc) => {
+    return dayjs(doc.data().createdAt).isAfter(dayjs(insertTime))
+  });
+
+  if(followingRequest.length > 0) {
+    response.send("特に動作なし");
+    return;
+  }
+
+  // 前のリクエストから動作を検証する
+  const previousRequests = resentDocs.docs.filter((doc) => dayjs(doc.data().createdAt).isBefore(dayjs(insertTime).subtract(3, "second")));
+
+  // リクエストがなかったら
+  response.send(previousRequests.length.toString());
+});
+
